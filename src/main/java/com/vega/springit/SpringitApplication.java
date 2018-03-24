b38cf89b4f53e75cae227e816317c3d39b8fb61c
Dan Vega <danvega@gmail.com>
2018-03-24 11:33:10 -0400

    Removing the welcome message. This was just used as a branching example.

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -8,6 +8,5 @@ public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-		System.out.println("Welcome to Springit!");
 	}
 }
