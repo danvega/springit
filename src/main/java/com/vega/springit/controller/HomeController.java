6dbfe8600571baf848fa0885d2ab2c6508aadc4d
Dan Vega <danvega@gmail.com>
2018-11-02 17:49:30 -0400

    Setting up thymeleaf and starting to add some views

--- src/main/java/com/vega/springit/controller/HomeController.java
+++ src/main/java/com/vega/springit/controller/HomeController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model, HttpServletRequest request){
-        model.addAttribute("message", "Hello World!");
-        return "index";
+    @GetMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title","Hello, Thymeleaf!");
+        return "home";
     }
 
 }
