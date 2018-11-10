4dcdff7b560e87e64fe8c9289074c9254e9237f2
Dan Vega <danvega@gmail.com>
2018-11-10 17:39:51 -0500

    Users & Roles

---
+++ src/main/java/com/vega/springit/domain/Role.java
@@ -0,0 +1,28 @@
+package com.vega.springit.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Collection;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @ManyToMany( mappedBy = "roles")
+    private Collection<User> users;
+
+}
---
+++ src/main/java/com/vega/springit/domain/User.java
@@ -0,0 +1,69 @@
+package com.vega.springit.domain;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NonNull
+    @Size(min = 8, max = 20)
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NonNull
+    @Column(length = 100)
+    private String password;
+
+    @NonNull
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
