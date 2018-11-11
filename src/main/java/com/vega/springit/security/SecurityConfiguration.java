3046cbb57198e905b5c78a5446779377ba28567e
Dan Vega <danvega@gmail.com>
2018-11-10 19:11:36 -0500

    Actuator endpoints

--- src/main/java/com/vega/springit/security/SecurityConfiguration.java
+++ src/main/java/com/vega/springit/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.vega.springit.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
            .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                .antMatchers("/actuator/").hasRole("ADMIN")
                 .antMatchers("/").permitAll()
                 .antMatchers("/link/submit").hasRole("USER")
            .and()
