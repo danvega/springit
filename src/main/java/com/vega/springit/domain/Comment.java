3ffc72a464a65f68701369216420186d9070499b
Dan Vega <danvega@gmail.com>
2018-11-15 17:04:10 -0500

    List comments out for each link

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,11 +1,16 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @RequiredArgsConstructor
@@ -26,4 +31,12 @@ public class Comment extends Auditable {
     @NonNull
     private Link link;
 
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 }
