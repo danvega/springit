bc8f1628ce7096f0df0d04c44e7c7faa75c98dab
Dan Vega <danvega@gmail.com>
2018-11-15 10:45:35 -0500

    Who is the user that submitted this link

--- src/main/java/com/vega/springit/repository/LinkRepository.java
+++ src/main/java/com/vega/springit/repository/LinkRepository.java
@@ -2,6 +2,9 @@ package com.vega.springit.repository;
 
 import com.vega.springit.domain.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LinkRepository extends JpaRepository<Link,Long> {
 
