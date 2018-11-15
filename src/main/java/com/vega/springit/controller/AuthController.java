880f838e4d5d976f09b92d5c295a50ace6c0b01c
Dan Vega <danvega@gmail.com>
2018-11-15 11:09:02 -0500

    Added account register and profile mappings and templates

--- src/main/java/com/vega/springit/controller/AuthController.java
+++ src/main/java/com/vega/springit/controller/AuthController.java
@@ -11,4 +11,14 @@ public class AuthController {
         return "auth/login";
     }
 
+    @GetMapping("/profile")
+    public String profile() {
+        return "auth/profile";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
+
 }
