7e5c0568f59572ab7c1fee1c96872b03509c465a
Dan Vega <danvega@gmail.com>
2018-12-03 11:56:52 -0500

    Creating our user service class

--- src/main/java/com/vega/springit/controller/AuthController.java
+++ src/main/java/com/vega/springit/controller/AuthController.java
@@ -1,10 +1,17 @@
 package com.vega.springit.controller;
 
+import com.vega.springit.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
     public String login() {
---
+++ src/main/java/com/vega/springit/service/UserService.java
@@ -0,0 +1,26 @@
+package com.vega.springit.service;
+
+import com.vega.springit.domain.User;
+import com.vega.springit.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user) {
+        return user;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
